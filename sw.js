0f218527b56a71b37310d79b061d299f111b2fc3
yannyn3 <143878999+yannyn3@users.noreply.github.com>
2025-03-01 22:28:33 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,71 @@
+// 简单的Service Worker示例
+const CACHE_NAME = 'food-scanner-v1';
+
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  '/wxpay.png',
+  '/alipay.png'
+];
+
+// 安装Service Worker
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// 激活Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 拦截网络请求
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 如果请求在缓存中找到了，就返回缓存的版本
+        if (response) {
+          return response;
+        }
+        
+        // 否则尝试从网络获取
+        return fetch(event.request)
+          .then(response => {
+            // 如果请求成功，克隆响应，存储到缓存中
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            const responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          });
+      })
+  );
+});
